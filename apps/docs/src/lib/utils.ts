9dbeef43beef738ec724ab5baa2cec87fcb84b90
gregogun <gregogun97@gmail.com>
2026-03-10 03:21:56 +0000

    feat: migrate to base

--- apps/docs/src/lib/utils.ts
+++ apps/docs/src/lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-// Migrated from packages/react/src/utils/dom.ts
-export const ariaAttr = (condition: boolean | undefined) =>
-  condition ? true : undefined
-
-export const dataAttr = (condition: boolean | undefined) =>
-  condition ? "" : undefined
