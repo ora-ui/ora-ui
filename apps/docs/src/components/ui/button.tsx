8e1424a6a28f0c6ca2a0e5df36cfdbb4168e1aa6
gregogun <gregogun97@gmail.com>
2026-03-23 08:23:03 +0000

    refactor(button): refine sizing and icon spacing

--- apps/docs/src/components/ui/button.tsx
+++ apps/docs/src/components/ui/button.tsx
@@ -31,7 +31,7 @@ const getThemeStyles = (theme: Theme): React.CSSProperties => {
 };
 
 const buttonVariants = cva(
-  "group/button inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all select-none bg-clip-padding disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0  focus-visible:outline-2 focus-visible:outline-(--btn-theme-500) aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "group/button inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-sm font-medium transition-all select-none bg-clip-padding disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0  focus-visible:outline-2 focus-visible:outline-(--btn-theme-500) aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -47,8 +47,8 @@ const buttonVariants = cva(
       },
       size: {
         sm: "h-8 rounded-sm gap-1.5 px-3 has-[>svg]:px-2.5 text-xs [&_svg:not([class*='size-'])]:size-3",
-        md: 'h-9 px-5 has-[>svg]:px-3',
-        lg: 'h-10 rounded-sm px-6 has-[>svg]:px-4',
+        md: 'h-7.5 px-3 has-[>svg]:px-2.5',
+        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
         'icon-sm': 'size-8',
         'icon-lg': 'size-10',
