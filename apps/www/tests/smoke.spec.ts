1836346d5cab3a5d9fe7376887a3d17b5f4cd55a
gregogun <gregogun97@gmail.com>
2026-06-11 00:58:49 +0100

    test: update smoke test to account for new homepage

--- apps/www/tests/smoke.spec.ts
+++ apps/www/tests/smoke.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 test('clicking on "Documentation" in primary nav navigates to documentation page', async ({
   page,
 }) => {
-  await page.goto('/');
+  await page.goto('/playground');
 
   const primaryNav = page.getByRole('navigation', { name: 'Primary navigation' });
   await primaryNav.getByRole('link', { name: 'Documentation' }).click();
